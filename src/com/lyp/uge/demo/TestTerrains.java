36097b8069b9a4dcd9a2347bdbe42760eec34a4a
Felix <984082450@qq.com>
2017-12-12 00:17:28 +0800

    fixed render terrain issues

--- src/com/lyp/uge/demo/TestTerrains.java
+++ src/com/lyp/uge/demo/TestTerrains.java
@@ -36,24 +36,27 @@ public class TestTerrains extends GameApplication {
 	@Override
 	protected void onCreate() {
 		enablePolygonMode();
-		getMainCamera().setSpeed(0.6f);
-		getMainCamera().setPosition(new Vector3f(0, 100, 0));
+		getMainCamera().setSpeed(0.7f);
+		getMainCamera().setPosition(new Vector3f(0, 5, 0));
 		
 		RawModel rawModel = OBJLoader.loadObjModel(DataUtils.OBJ_TREE, loader);
 		Texture texture = loader.loadTexture("res/texture/" + DataUtils.TEX_TREE);
-		texture.setShineDamper(10.0f);	//设置反射光亮度衰减因子
-		texture.setReflectivity(1.0f);	//设置反射光反射率因子
+		texture.setShineDamper(8.0f);	//设置反射光亮度衰减因子
+		texture.setReflectivity(0.8f);	//设置反射光反射率因子
 		textureModel = new TextureModel(rawModel, texture);
-		light = new Light(new Vector3f(-100.0f, 100.0f, -100.0f), new Vector3f(1, 1, 1), loader);
+		light = new Light(new Vector3f(0.0f, 1000.0f, -500.0f), new Vector3f(1, 1, 1), loader);
 		objects = new DemoObject[100];
 		for (int i = 0; i < objects.length; i++) {
 			objects[i] = new DemoObject(textureModel, new Vector3f(
 					random.nextFloat() * 200 - 100, 0, -random.nextInt(250)), 0f, 0f, 0f, 1.5f +random.nextFloat()*2);
 		}
 		
+		Texture textureTerrain = loader.loadTexture("res/texture/" + DataUtils.TEX_GRASS);
+		textureTerrain.setShineDamper(10.0f);	//设置反射光亮度衰减因子
+		textureTerrain.setReflectivity(0.7f);	//设置反射光反射率因子
 		terrains = new Terrain[2];
-		terrains[0] = new Terrain(0, -1, loader, loader.loadTexture("res/texture/" + DataUtils.TEX_GRASS2));
-		terrains[1] = new Terrain(-1, -1, loader, loader.loadTexture("res/texture/" + DataUtils.TEX_GRASS));
+		terrains[0] = new Terrain(0, -1, loader, textureTerrain);
+		terrains[1] = new Terrain(-1, -1, loader, textureTerrain);
 		
 		rendererManager = new RendererManager();
 	}
@@ -61,11 +64,12 @@ public class TestTerrains extends GameApplication {
 	@Override
 	protected void onUpdate() {
 		light.update();
+		Logger.d("Lighting", light.getPosition().toString());
 	}
 	
 	@Override
 	protected void onRender() {
-		rendererManager.prepare(0f, 0f, 0.3f, 1f);
+		rendererManager.prepare(0.5f, 0.8f, 0.95f, 1.0f);
 		for (int i = 0; i < objects.length; i++) {
 			rendererManager.addObject(objects[i]);
 		}
