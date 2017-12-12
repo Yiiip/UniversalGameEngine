362ed85345902b90a93401d94fdf5bc74b31336d
Felix <984082450@qq.com>
2017-12-12 23:57:23 +0800

    render alpha channel on grass_real texture

--- src/com/lyp/uge/demo/TestTerrains.java
+++ src/com/lyp/uge/demo/TestTerrains.java
@@ -19,8 +19,9 @@ import com.lyp.uge.utils.DataUtils;
 public class TestTerrains extends GameApplication {
 
 	private Loader loader = new Loader();
-	private TextureModel textureModel;
-	private DemoObject[] objects;
+	private DemoObject[] oTrees;
+	private DemoObject[] oGrasses;
+	private DemoObject[] oFerns;
 	private Terrain[] terrains;
 	private Light light;
 	private RendererManager rendererManager;
@@ -39,20 +40,43 @@ public class TestTerrains extends GameApplication {
 		getMainCamera().setSpeed(0.7f);
 		getMainCamera().setPosition(new Vector3f(0, 5, 0));
 		
+		light = new Light(new Vector3f(0.0f, 1000.0f, -500.0f), new Vector3f(1, 1, 1), loader);
+
+		//树木
 		RawModel rawModel = OBJLoader.loadObjModel(DataUtils.OBJ_TREE, loader);
 		Texture texture = loader.loadTexture("res/texture/" + DataUtils.TEX_TREE);
 		texture.setShineDamper(8.0f);	//设置反射光亮度衰减因子
 		texture.setReflectivity(0.8f);	//设置反射光反射率因子
-		textureModel = new TextureModel(rawModel, texture);
-		
-		light = new Light(new Vector3f(0.0f, 1000.0f, -500.0f), new Vector3f(1, 1, 1), loader);
-		
-		objects = new DemoObject[1000];
-		for (int i = 0; i < objects.length; i++) {
-			objects[i] = new DemoObject(textureModel, new Vector3f(
-					random.nextFloat() * Terrain.SIZE*2 - Terrain.SIZE, 0, -random.nextInt((int) Terrain.SIZE)), 0f, 0f, 0f, 1.5f +random.nextFloat()*2);
+		TextureModel textureModel = new TextureModel(rawModel, texture);
+		oTrees = new DemoObject[1000];
+		for (int i = 0; i < oTrees.length; i++) {
+			oTrees[i] = new DemoObject(textureModel, new Vector3f(
+					random.nextFloat() * Terrain.SIZE*2 - Terrain.SIZE, 0, -random.nextInt((int) Terrain.SIZE)), 0f, 0f, 0f, 2.0f +random.nextFloat()*2);
 		}
 		
+		//草类植物
+		RawModel grassRawModel = OBJLoader.loadObjModel(DataUtils.OBJ_GRASS_REAL, loader);
+		Texture grassTexture = loader.loadTexture("res/texture/" + DataUtils.TEX_GRASS_REAL);
+		grassTexture.setShineDamper(8.0f);
+		grassTexture.setReflectivity(2.0f);
+		TextureModel grassTextureModel = new TextureModel(grassRawModel, grassTexture);
+		oGrasses = new DemoObject[1500];
+		for (int i = 0; i < oGrasses.length; i++) {
+			oGrasses[i] = new DemoObject(grassTextureModel, new Vector3f(
+					random.nextFloat() * Terrain.SIZE*2 - Terrain.SIZE, 0, -random.nextInt((int) Terrain.SIZE)-10.0f), 0f, 0f, 0f, random.nextFloat()+0.05f);
+		}
+		RawModel fernRawModel = OBJLoader.loadObjModel(DataUtils.OBJ_FERN, loader);
+		Texture fernTexture = loader.loadTexture("res/texture/" + DataUtils.TEX_FERN);
+		fernTexture.setShineDamper(10.0f);
+		fernTexture.setReflectivity(1.0f);
+		TextureModel fernTextureModel = new TextureModel(fernRawModel, fernTexture);
+		oFerns = new DemoObject[1000];
+		for (int i = 0; i < oFerns.length; i++) {
+			oFerns[i] = new DemoObject(fernTextureModel, new Vector3f(
+					random.nextFloat() * Terrain.SIZE*2 - Terrain.SIZE, 0, -random.nextInt((int) Terrain.SIZE)-10.0f), 0f, 0f, 0f, random.nextFloat()+0.04f);
+		}
+		
+		//地形
 		Texture textureTerrain = loader.loadTexture("res/texture/" + DataUtils.TEX_GRASS);
 		textureTerrain.setShineDamper(10.0f);
 		textureTerrain.setReflectivity(0.7f);
@@ -72,8 +96,14 @@ public class TestTerrains extends GameApplication {
 	@Override
 	protected void onRender() {
 		rendererManager.prepare(0.5f, 0.8f, 0.95f, 1.0f);
-		for (int i = 0; i < objects.length; i++) {
-			rendererManager.addObject(objects[i]);
+		for (int i = 0; i < oTrees.length; i++) {
+			rendererManager.addObject(oTrees[i]);
+		}
+		for (int i = 0; i < oGrasses.length; i++) {
+			rendererManager.addObject(oGrasses[i]);
+		}
+		for (int i = 0; i < oFerns.length; i++) {
+			rendererManager.addObject(oFerns[i]);
 		}
 		rendererManager.addTerrain(terrains[0]);
 		rendererManager.addTerrain(terrains[1]);
--- src/com/lyp/uge/utils/DataUtils.java
+++ src/com/lyp/uge/utils/DataUtils.java
@@ -5,7 +5,7 @@ public class DataUtils {
 	public static final String OBJ_DRAGON = "dragon.obj";
 	public static final String OBJ_RABBIT = "rabbit.obj"; //斯坦福兔
 	public static final String OBJ_TREE = "tree.obj";
-	public static final String OBJ_LOW_POLY_TREE = "lowPolyTree.obj";
+	public static final String OBJ_TREE_BIG = "treeBig.obj";
 	public static final String OBJ_CUBE = "cube.obj";
 	public static final String OBJ_CUBE2 = "cube2.obj";
 	public static final String OBJ_SPHERE_LOW_QUALITY = "sphere_low_quality.obj";
@@ -23,12 +23,12 @@ public class DataUtils {
 	public static final String TEX_COLOR_LIGHT_GRAY = "color_light_gray.png";
 	public static final String TEX_GRASS = "grass.png";
 	public static final String TEX_GRASS2 = "grass2.png";
+	public static final String TEX_GRASS_REAL = "grass_real.png";
 	public static final String TEX_IMAGE_PREVIEW = "image_preview.png";
 	public static final String TEX_MC_DIRT_GRASS = "mc_dirt_grass.png";
 	public static final String TEX_MC_CUBE = "mc_cube_texture.png";
 	public static final String TEX_TREE = "tree.png";
-	public static final String TEX_LOW_POLY_TREE = "lowPolyTree.png";
-	public static final String TEX_GRASS_REAL = "grass_real.png";
+	public static final String TEX_TREE_BIG = "treeBig.png";
 	public static final String TEX_FERN = "fern.png";
 	public static final String TEX_FLOWER = "flower.png";
 	public static final String TEX_STALL = "stallTexture.png";
