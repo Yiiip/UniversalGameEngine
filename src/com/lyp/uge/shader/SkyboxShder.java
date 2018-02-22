5ab87850e3a5f0a4d907dd0784260c1f7d094b97
Felix <984082450@qq.com>
2018-02-22 13:48:08 +0800

    Rotate skybox automatically.

--- src/com/lyp/uge/shader/SkyboxShder.java
+++ src/com/lyp/uge/shader/SkyboxShder.java
@@ -10,28 +10,33 @@ public class SkyboxShder extends StaticShader {
 
 	private static String SKYBOX_VERT = "shader/skybox.vert";
 	private static String SKYBOX_FRAG = "shader/skybox.frag";
-	
-	protected int uniform_fogColor; //(= skyColor)
-	
+
+	protected int uniform_fogColor; // (= skyColor)
+
+	protected static float rotateSpeed = 1.f;
+	protected float rotation = 0.f;
+
 	public SkyboxShder() {
 		super(SKYBOX_VERT, SKYBOX_FRAG);
 	}
-	
+
 	@Override
 	protected void getAllUniformLocations() {
 		super.getAllUniformLocations();
 		uniform_fogColor = super.getUniformLocation("fogColor");
 	}
-	
+
 	@Override
 	public void loadViewMatrix(Camera camera) {
 		Matrix4f viewMatrix = MathTools.createViewMatrix(camera);
 		viewMatrix.m30 = 0;
 		viewMatrix.m31 = 0;
 		viewMatrix.m32 = 0;
+		rotation += rotateSpeed * (1.f / 60.f); //TODO (speed * current fps)
+		Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0, 1, 0), viewMatrix, viewMatrix);
 		super.loadMatrix(uniform_viewMatrix, viewMatrix);
 	}
-	
+
 	public void setupFogColor(float r, float g, float b) {
 		super.loadVector(uniform_fogColor, new Vector3f(r, g, b));
 	}
