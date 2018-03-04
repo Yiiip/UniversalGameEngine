7be289c8330f2cddf33082e03f91d2c5e09d1eb5
Felix <984082450@qq.com>
2018-03-05 00:29:45 +0800

    Update FpsCounterView with resolution info.

--- src/com/lyp/uge/gui/widget/FpsCounterView.java
+++ src/com/lyp/uge/gui/widget/FpsCounterView.java
@@ -10,16 +10,17 @@ import com.lyp.uge.window.Window;
 
 public class FpsCounterView extends Widget {
 
-	private static final String TEXT_FPS = "FPS:";
-	private static final String TEXT_UPS = " | UPS:";
-	private static final String TEXT_RUNTIME = "Running: ";
+	private static final String TEXT_FPS		= "FPS:";
+	private static final String TEXT_UPS		= " | UPS:";
+	private static final String TEXT_RUNTIME	= "Running: ";
+	private static final String TEXT_RESOLUTION	= "Windowed: ";
 
 	private float x;
 	private float y;
 	private float w = 220.0f;
 	private float h = 100.0f;
 	private float padding = 12.0f;
-	private float fSize = 22.0f;
+	private float fSize = 20.0f;
 	private final String fName = "RobotoMono-Regular";
 	private ByteBuffer fRes = loadResource(DataUtils.FONT_ROBOTO_MONO_REGULAR, 150 * 1024);
 	private NVGColor color = NVGColor.create();
@@ -54,7 +55,8 @@ public class FpsCounterView extends Widget {
 		nvgTextAlign(ctx, NVG_ALIGN_LEFT | NVG_ALIGN_TOP);
 		nvgFillColor(ctx, rgba(139, 229, 165, 255, color));
 		nvgText(ctx, x + padding, y + padding, TEXT_FPS + fps + TEXT_UPS + ups);
-		nvgText(ctx, x + padding, y + fSize + padding, TEXT_RUNTIME + runtime);
+		nvgText(ctx, x + padding, y + fSize * 1 + padding, TEXT_RUNTIME + runtime);
+		nvgText(ctx, x + padding, y + fSize * 2 + padding, TEXT_RESOLUTION + window.getWidth() + "×" + window.getHeight());
 	}
 
 	@Override
